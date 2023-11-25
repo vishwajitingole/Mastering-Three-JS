cb74f69d66126c2bd693a23a169004eaedbbadac
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-11-25 14:49:18 +0530

    camera positioning

--- name/src/App.js
+++ name/src/App.js
@@ -5,7 +5,7 @@ import Scene from "./Scene";
 function App() {
   return (
     <div className="container">
-      <Canvas>
+      <Canvas camera={{ fov: 45, near: 0.1, far: 100, position: [2, 2, 6] }}>
         <Scene />
       </Canvas>
     </div>
